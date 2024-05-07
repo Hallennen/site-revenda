6dc2dcdaecbe5e6f918d42a01893e164c6f7073e
Hallennen <hallennen.marinho3@gmail.com>
2024-05-07 15:46:23 -0300

    feature edit / fix active and desactive

--- main.py
+++ main.py
@@ -10,21 +10,29 @@ app = Flask(__name__)
 def dados_banco(dados_da_busca=['completo','especifico' and 'like'], tipo=''):
     global valor, dados
     if dados_da_busca == 'completo':
-        script = f''' SELECT * FROM produtos_shekina ORDER BY tipo_produto '''
+        script = f''' SELECT * FROM produtos_shekina WHERE ativo = 'SIM' ORDER BY tipo_produto ASC'''
         dados = conecta_no_banco.connect_banco(script= script,bd='shekina', opção= 'fetchall')
     
     elif dados_da_busca == 'especifico':
-        script = f''' SELECT * FROM produtos_shekina WHERE tipo_produto = '{tipo}'  '''
+        script = f''' SELECT * FROM produtos_shekina WHERE ativo = 'SIM' AND tipo_produto = '{tipo}'  '''
         dados = conecta_no_banco.connect_banco(script= script,bd='shekina', opção= 'fetchall')
     
     elif dados_da_busca == 'like':
-        script = f''' select * from produtos_shekina WHERE descricao_item LIKE '%{tipo}%'  '''
+        script = f''' select * from produtos_shekina WHERE ativo = 'SIM' AND descricao_item LIKE '%{tipo}%'  '''
         dados = conecta_no_banco.connect_banco(script= script,bd='shekina', opção= 'fetchall')
-
-
 
     valor = len(dados)
     return dados
+
+
+
+def contador(ativo=['SIM', 'NAO']):      
+          
+    script = script = f''' SELECT COUNT(id_item) FROM produtos_shekina WHERE ativo = '{ativo}' '''
+    dados = conecta_no_banco.connect_banco(script= script,bd='shekina', opção= 'fetchone')
+    
+
+    return dados[0]
 
 
 
@@ -50,11 +58,15 @@ def buscar_catalogo_por_pesquisa():
     return render_template('homepage.html', produto= dados_banco('like',produto), lista_num = int(valor))
 
 
+
+
 @app.route('/catalogo/<produto>')
 def buscar_catalogo_por_produto(produto):
 
 
     return render_template('homepage.html', produto= dados_banco('especifico',produto), lista_num = int(valor))
+
+
 
 
 @app.route('/detalhe/<produto>')
@@ -66,7 +78,7 @@ def detalhe_produto(produto):
 
 
 
-    return render_template('detalhe.html', detalhe_descricao_produto = detalhe_item[1] , detalhe_valor_produto = detalhe_item[3], detalhe_imagem_produto = detalhe_item[2])
+    return render_template('detalhe.html', teamplate = 'base', detalhe_descricao_produto = detalhe_item[1] , detalhe_valor_produto = detalhe_item[3], detalhe_imagem_produto = detalhe_item[2])
 
 
 
@@ -83,13 +95,14 @@ def login_admin():
 
 
 
-@app.route('/validacao', methods=['GET', 'POST'])
+@app.route('/validacao', methods=['POST'])
 def pegar_credenciais():
     global senha, email
     email = request.form['email_admin']
     senha = request.form['senha_admin']
 
     return valida_login(email , senha)
+
 
 def valida_login(email, senha):
     try:
@@ -112,24 +125,110 @@ def valida_login(email, senha):
 
 @app.route('/cadastro-produto', methods=['GET','POST'])
 def cadastroPage():
-  
+
+    script = f''' SELECT * FROM produtos_shekina WHERE ativo = 'SIM' ORDER BY tipo_produto  '''
+
+    lista_de_itens_cadastrados_ativos = conecta_no_banco.connect_banco(script=script,bd='shekina',opção='fetchall')
     
-    
-    return render_template('cadastro.html')
+    script = f''' SELECT * FROM produtos_shekina WHERE ativo = 'NAO' '''
+
+    lista_de_itens_cadastrados_desativados = conecta_no_banco.connect_banco(script=script,bd='shekina',opção='fetchall')
 
 
-@app.route('/cadastro-produto=true', methods=['GET','POST'])
+    return render_template('cadastro.html', lista_de_itens_cadastrados_ativos= lista_de_itens_cadastrados_ativos, 
+                           lista_de_itens_cadastrados_desativados= lista_de_itens_cadastrados_desativados, 
+                           numero_ativos = contador(ativo='SIM'), 
+                           numero_desativados = contador(ativo='NAO')  )
+
+
+
+
+@app.route('/cadastro-produto=true', methods=['POST'])
 def cadastro():
     novo_item_descricao = request.form['novo_item_descricao']
     novo_item_link = request.form['novo_item_link']
     novo_item_valor = request.form['novo_item_valor']
     novo_item_tipo = request.form['novo_item_tipo']
-    script = f''' INSERT INTO produtos_shekina (descricao_item, link_imagem_item, valor_item, tipo_produto) 
-                VALUES ('{novo_item_descricao}','{novo_item_link}','{novo_item_valor}','{novo_item_tipo}') '''
+    novo_item_ativo = request.form['novo_item_ativo']
+    script = f''' INSERT INTO produtos_shekina (descricao_item, link_imagem_item, valor_item, tipo_produto, ativo) 
+                VALUES ('{novo_item_descricao}','{novo_item_link}','{novo_item_valor}','{novo_item_tipo}', '{novo_item_ativo}') '''
     conecta_no_banco.connect_banco(script=script,bd='shekina',opção='insert')
 
-    return render_template('cadastro.html')
+    return redirect('/cadastro-produto')
 
 
 
-app.run(host='localhost', port=9001, debug=True)
+
+@app.route('/ativacao',  methods=['POST'])
+def ativacao():
+    item_id_para_ativar = request.form['id_item_a_ativar']
+    try:
+        script = f''' UPDATE produtos_shekina SET ativo = 'SIM' WHERE id_item = '{item_id_para_ativar}' '''
+        conecta_no_banco.connect_banco(script=script, bd='shekina',opção='update')
+    
+    except:
+        print('Não atualizou')
+
+    return redirect('/cadastro-produto')
+
+
+
+
+@app.route('/desativacao', methods=['POST'])
+def desativacao():
+    item_id_para_desativar = request.form['id_item_a_desativar']
+    try:
+        script= f''' UPDATE produtos_shekina SET ativo = 'NAO' WHERE id_item = '{item_id_para_desativar}'  '''
+        conecta_no_banco.connect_banco(script, 'shekina', 'update')
+    except:
+        print('Não atualizou')
+
+
+    return redirect('/cadastro-produto')
+
+
+
+
+id_respawn = []
+
+@app.route('/edicao', methods=['POST'])
+def edicao():
+    global id_item
+
+    try:
+        id_item = request.form['id_item_a_editar']
+        id_respawn.insert(0,int(id_item))
+        print(id_respawn)
+    except:
+        id_item = id_respawn[0]
+
+
+    script= f''' SELECT * FROM produtos_shekina WHERE id_item = '{id_item}' '''
+
+    detalhe_item = conecta_no_banco.connect_banco(script=script, bd='shekina',opção='fetchone')
+
+    return render_template(['detalhe.html','edicao.html'], teamplate = 'edicao', detalhe_descricao_produto = detalhe_item [1], detalhe_imagem_produto = detalhe_item[2], detalhe_valor_produto= detalhe_item[3], infos = detalhe_item)
+
+
+
+
+@app.route('/edicao=True', methods=['POST'])
+def edicao_true():
+    descricao_atualizada = request.form['descricao']
+    link_atualizado = request.form['link']
+    valor_atualizado = request.form['valor']
+    tipo_atualizado = request.form['tipo']
+
+    script = f'''UPDATE produtos_shekina SET descricao_item = '{descricao_atualizada}', link_imagem_item = '{link_atualizado}', valor_item = '{valor_atualizado}', tipo_produto = '{tipo_atualizado}' WHERE id_item = '{id_item}' '''
+    atualizacao = conecta_no_banco.connect_banco(script, 'shekina', 'update')
+
+    return edicao()
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=9001, debug=True)
+
+
